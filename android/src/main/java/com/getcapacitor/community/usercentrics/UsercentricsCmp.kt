ba1682c85e05fb12d68fc76dd93297a50ad31bb9
Matthias Böhme <matthias@prototype.berlin>
2021-04-01 12:36:10 +0200

    [android] intialize and present cmp, handle consent data properly

--- android/src/main/java/com/getcapacitor/community/usercentrics/UsercentricsCmp.kt
+++ android/src/main/java/com/getcapacitor/community/usercentrics/UsercentricsCmp.kt
@@ -1,65 +1,118 @@
 package com.getcapacitor.community.usercentrics;
 
+import android.util.Log
 import com.getcapacitor.*
-import com.usercentrics.sdk.*
-import com.usercentrics.sdk.models.common.*
+import com.usercentrics.sdk.Usercentrics
+import com.usercentrics.sdk.UsercentricsActivity
+import com.usercentrics.sdk.models.common.InitialView
+import com.usercentrics.sdk.models.common.UserOptions
 import org.json.JSONArray
 
 @NativePlugin
 class UsercentricsCmp : Plugin() {
-    @PluginMethod
-    fun getPermissions(call: PluginCall) {
-      val settingsId = call.getString("settingsId", "")
-      if (settingsId == "")
-      {
-        call.reject("settingsId missing")
-        return
-      }
+  private val TAG = "UsercentricsCmp"
 
-        val acceptedVendors = arrayListOf<String?>()
-        val acceptedCategories = arrayListOf<String?>()
-        val permissions = JSObject()
-        permissions.put("acceptedVendors", JSONArray(acceptedVendors))
-        permissions.put("acceptedCategories", JSONArray(acceptedCategories))
-        call.resolve(permissions)
+  private val userOptions = UserOptions(
+    controllerId = null,
+    defaultLanguage = "de",
+    version = null,
+    debugMode = true,
+    predefinedUI = true,
+    timeoutMillis = 10000,
+    noCache = false
+  )
 
-        val userOptions = UserOptions();
-        val usercentrics = Usercentrics(
-          settingsId = settingsId,
-          options = userOptions,
-          appContext = context // Create your Usercentrics instance  within your Application or Activity class.
-        )
-
-        usercentrics.initialize(
-          callback = { initialValues ->
-            call.resolve(permissions);
-          },
-          onFailure = { error ->
-            call.reject("error");
-          }
-        )
+  @PluginMethod
+  fun getPermissions(call: PluginCall) {
+    val settingsId = call.getString("settingsId", "")
+    if (settingsId == "") {
+      call.reject("settingsId missing")
+      return
     }
+
+    Log.d(TAG, settingsId);
+
+    val usercentrics = Usercentrics(
+      settingsId = settingsId,
+      options = userOptions,
+      appContext = context
+    )
+
+    usercentrics.initialize(
+      callback = { initialValues ->
+        when (initialValues.initialLayer) {
+          InitialView.FIRST_LAYER -> presentCMP(call, settingsId, usercentrics)
+          InitialView.NONE -> {
+            updateConsents(call, settingsId, usercentrics)
+          }
+        }
+
+      },
+      onFailure = { error ->
+        Log.e(TAG, error.toString());
+        call.reject("error");
+      }
+    )
+  }
 
 
   @PluginMethod
   fun setPermissions(call: PluginCall) {
     val settingsId = call.getString("settingsId", "")
-    if (settingsId == "")
-    {
+    if (settingsId == "") {
       call.reject("settingsId missing")
       return
     }
     val permissions = call.getObject("permissions", JSObject())
     call.resolve()
   }
+
   @PluginMethod
   fun reset(call: PluginCall) {
     val settingsId = call.getString("settingsId", "")
-    if (settingsId == "")
-    {
+    if (settingsId == "") {
       call.reject("settingsId missing")
       return
     }
     call.resolve()
+  }
+
+  private fun presentCMP(call: PluginCall, settingsId: String, usercentrics: Usercentrics) {
+    Log.d(TAG, "present cmp");
+
+    UsercentricsActivity.start(
+      context = context,
+      settingsID = settingsId,
+      userOptions = userOptions
+    )
+  }
+
+  private fun updateConsents(call: PluginCall, settingsId: String, usercentrics: Usercentrics) {
+    val acceptedCategories = arrayListOf<String?>()
+    val acceptedServices = arrayListOf<String?>()
+    val consents = JSObject()
+
+    // set accepted categories
+    val categories = usercentrics.getCategories();
+    if (categories != null) {
+      for (category in categories) {
+        acceptedCategories.add(category.slug);
+      }
+    }
+
+    // set accepted services
+    val services = usercentrics.getServices();
+    if (services != null) {
+      for (service in services) {
+        acceptedServices.add(service.id);
+      }
+    }
+
+    consents.put("acceptedCategories", JSONArray(acceptedCategories))
+    consents.put("acceptedVendors", JSONArray(acceptedServices))
+
+    Log.d(TAG, "get permissions");
+    Log.d(TAG, consents.toString());
+    call.resolve(consents)
   }
 }
