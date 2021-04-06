484792a003e8875b1be6bdd00ffe20b10ea8738a
Matthias Böhme <matthias@prototype.berlin>
2021-04-06 17:38:18 +0200

    provide mocked state "accept all vendors" for web implementation

--- src/web.ts
+++ src/web.ts
@@ -2,7 +2,38 @@ import { WebPlugin, registerWebPlugin } from '@capacitor/core';
 import { UsercentricsCmpPlugin, UsercentricsConsents } from './definitions';
 
 const MOCK_CONSENTS: UsercentricsConsents = {
-  acceptedVendors: [],
+  acceptedVendors: [
+    {
+      'id': 'Afk_Nife_',
+      'label': 'Adjust',
+      'categoryId': 'marketing',
+      'subVendors': [],
+    },
+    {
+      'id': 'MbHgRDHhR',
+      'label': 'Firebase (ohne Analysefunktion)',
+      'categoryId': 'essential',
+      'subVendors': [],
+    },
+    {
+      'id': 'yBaAnQfrt',
+      'label': 'Google Firebase',
+      'categoryId': 'marketing',
+      'subVendors': [],
+    },
+    {
+      'id': '5bG1RVWaN',
+      'label': 'The Adex',
+      'categoryId': 'marketing',
+      'subVendors': [],
+    },
+    {
+      'id': 'H1Vl5NidjWX',
+      'label': 'Usercentrics Consent Management Platform',
+      'categoryId': 'essential',
+      'subVendors': [],
+    },
+  ],
 };
 
 export class UsercentricsCmpWeb extends WebPlugin implements UsercentricsCmpPlugin {
@@ -15,21 +46,21 @@ export class UsercentricsCmpWeb extends WebPlugin implements UsercentricsCmpPlug
 
   public async getConsents(options: { settingsId: string }): Promise<UsercentricsConsents> {
     console.log(`get consents for ${options.settingsId}`);
-    console.error('Usercentrics plugin not implemented for web. Return empty array of accepted vendors');
+    console.error('Usercentrics plugin not implemented for web. Mock state "accept all vendors"');
 
     return Promise.resolve(MOCK_CONSENTS);
   }
 
   public async updateConsents(options: { settingsId: string }): Promise<UsercentricsConsents> {
     console.log(`update consents for ${options.settingsId}`);
-    console.error('Usercentrics plugin not implemented for web. Return empty array of accepted vendors');
+    console.error('Usercentrics plugin not implemented for web. Mock state "accept all vendors"');
 
     return Promise.resolve(MOCK_CONSENTS);
   }
 
   public async resetConsents(options: { settingsId: string }): Promise<UsercentricsConsents> {
     console.log(`reset consents for ${options.settingsId}`);
-    console.error('Usercentrics plugin not implemented for web. Return empty array of accepted vendors');
+    console.error('Usercentrics plugin not implemented for web. Mock state "accept all vendors"');
 
     return Promise.resolve(MOCK_CONSENTS);
   }
