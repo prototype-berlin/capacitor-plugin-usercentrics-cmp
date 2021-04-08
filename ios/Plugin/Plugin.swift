689bbff764b7118abb38aea6ac6a64f6f970d74b
Matthias Böhme <matthias@prototype.berlin>
2021-04-08 13:46:45 +0200

    [ios] present usercentrics cmp if necessary

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,50 +8,82 @@ import Usercentrics
  */
 @objc(UsercentricsCmp)
 public class UsercentricsCmp: CAPPlugin {
+    var predefinedUI: UIViewController?
+    
     @objc override public func load() {
       // Called when the plugin is first constructed in the bridge
       print("load usercentrics cmp plugin");
     }
 
     @objc func getConsents(_ call: CAPPluginCall) {
-      guard let settingsId = call.options["settingsId"] as? String else {
-        call.reject("settingsId missing")
-        return
-      }
-
-      let userOptions = UserOptions(predefinedUI: true)
-
-      let usercentrics = Usercentrics(settingsId: settingsId,
-                                      options: userOptions)
-
-      usercentrics.initialize { initialValues in
-          call.resolve([
-                    "acceptedVendors": [],
-                ])
-      } onFailure: { error in
-        call.reject("ERROR")
-      }
+        guard let settingsId = call.options["settingsId"] as? String else {
+            call.reject("settingsId missing")
+            return
+        }
+        
+        DispatchQueue.main.async {
+            let userOptions = UserOptions(controllerId: nil,
+                                          defaultLanguage: nil,
+                                          version: nil,
+                                          debugMode: false,
+                                          predefinedUI: true,
+                                          timeoutMillis: nil,
+                                          noCache:false)
+            
+            let usercentrics = Usercentrics(settingsId: settingsId, options: userOptions)
+            usercentrics.initialize { initialValues in
+                switch initialValues.initialLayer {
+                case .firstLayer:
+                    self.presentCmp(call, usercentrics)
+                case .none:
+                    self.mapConsents(call)
+                default:
+                    print(initialValues.initialLayer)
+                }
+            } onFailure: { error in
+                print(error);
+            }
+        }
     }
-
+    
     @objc func updateConsents(_ call: CAPPluginCall) {
-      guard let settingsId = call.options["settingsId"] as? String else {
-        call.reject("settingsId missing")
-        return
-      }
-
+        guard let settingsId = call.options["settingsId"] as? String else {
+            call.reject("settingsId missing")
+            return
+        }
+        
         call.resolve([
-          "acceptedVendors": [],
+            "acceptedVendors": [],
         ])
     }
-
+    
     @objc func resetConsents(_ call: CAPPluginCall) {
-      guard let settingsId = call.options["settingsId"] as? String else {
-        call.reject("settingsId missing")
-        return
-      }
-
+        guard let settingsId = call.options["settingsId"] as? String else {
+            call.reject("settingsId missing")
+            return
+        }
+        
         call.resolve([
-          "acceptedVendors": [],
+            "acceptedVendors": [],
+        ])
+    }
+    
+    func presentCmp(_ call: CAPPluginCall, _ usercentrics: Usercentrics?) {
+        self.predefinedUI = usercentrics?.getPredefinedUI(customAssets: nil, showCloseButton: false) {
+            self.predefinedUI?.dismiss(animated: true, completion: nil)
+            self.mapConsents(call)
+        }
+        
+        guard let ui = self.predefinedUI else { return }
+        
+        self.bridge.viewController.present(ui, animated: true, completion: nil)
+    }
+    
+    func mapConsents(_ call: CAPPluginCall) {
+        print("TODO: map consents properly");
+        
+        call.resolve([
+            "acceptedVendors": [],
         ])
     }
 }
